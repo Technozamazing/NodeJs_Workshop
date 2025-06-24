c1156ab37c44d2787be65446713a8de05f8ea551
saugat1070 <saugatgiri1070@gmail.com>
2025-06-25 01:56:29 +0545

    javascript basic concept

--- day1/day1.js
+++ day1/day1.js
@@ -1,1 +1,38 @@
-console.log("Hi i am Cots");
+//variable 
+
+let something = "saugat";
+var something_2 = 10;
+const somethingSomething = "k cha"
+//operator
+const numberOne = 15;
+const numberTwo = "15";
+// console.log(numberOne == numberTwo);
+// console.log(typeof numberOne,typeof numberTwo)
+// console.log(numberOne === numberTwo)
+
+const textArea = numberOne == numberTwo ? "something is turn on true" : "may be it called on false";
+console.log(textArea)
+
+// JavaScript Type Operators
+//  --> typeof
+const operatorName = [typeof String,typeof Number,typeof Math, typeof new Date()];
+console.log(operatorName)
+
+// --> instanceof
+// Example: Check if a date object is an instance of Date
+const dateObj = new Date();
+console.log(dateObj instanceof Date); // true
+
+//bitwise operator
+
+console.log(5&1);
+console.log(5 | 1);
+console.log(~5);
+console.log(5^1);
+
+// arithmetic operator
+
+ let value = 2 ** 3;
+ console.log(value)
+ let valueFrom = Math.pow(2,3);
+ console.log(value,valueFrom);
