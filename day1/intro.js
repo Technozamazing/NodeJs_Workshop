28d4415522619106cf1aa2f1577cbbe80112328a
Goivnd Kumar Yadav <robertgovind@gmail.com>
2025-07-01 16:34:16 +0545

    first day source codr

---
+++ day1/intro.js
@@ -0,0 +1,58 @@
+// Hoisting: Variables declared with var are hoisted (moved to top of scope) and initialized with undefined
+// console.log(a);
+// var a = 4;
+// console.log(a);
+
+// console.log(a);
+// let a;
+// console.log(a);
+
+// const a = 3;
+
+// const date = new Date();
+// console.log(date instanceof Date);
+
+// var name = "ram";
+// console.log(name);
+// console.log(typeof name);
+
+// name = 5;
+
+// let mero_name = "saugat";
+// let meroNumber = "1070";
+
+// console.log(typeof meroNumber);
+
+// const a=9;
+
+// console.log(age);
+// var age = 19;
+// console.log(age);
+
+// let number1 = "1070";
+// let number2 = 1070;
+// console.log(number1 == number2); //true
+// console.log(typeof number1, typeof number2);
+// console.log(number1 === number2); //false
+
+// console.log(2 ** 3);
+// console.log(Math.pow(2, 3));
+
+// console.log("Ram" + " Kumar");
+
+// let name = "Shyam";
+// console.log(`My name is ${name}`);
+// let age = 20;
+// console.log(`My age is ${age}`);
+
+if (true) {
+  console.log("i am pro coder");
+} else {
+  console.log("I don't know how to code");
+}
+let anyNumber = 10;
+anyNumber % 2 == 0
+  ? console.log("number is even")
+  : console.log("number is odd");
+
+//condition ? true : false;
